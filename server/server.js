464eeb05ee621704945ca01d041f2ba5d4d87c87
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2017-03-11 12:52:13 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.use('/static', express.static('bower_components'));
+app.use('/file', express.static('pdfs'));
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+   res.send('use /pdf/<filename>');
+});
+
+app.get('/pdf/:filename', function(req, res) {
+   res.render('index', { title: 'PDF file', file: req.params.filename });
+});
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+});
