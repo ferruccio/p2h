347999a47ac1e07eed6bdfc73e45f8c0cad27659
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2017-03-12 16:14:09 -0400

    Add webpage-diag.js module
    Encapsulate webpage object with optional diagnostics.

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,11 +1,10 @@
-var page = require('webpage').create();
-var fs = require('fs');
-var system = require('system');
+var page = require('./webpage-diag.js').create({
+    load: pageLoaded
+});
 
-function finishLoad(status) {
-    if (status === "success") {
+function pageLoaded(success) {
+    if (success) {
         page.evaluate(function() {
-            var $ = $ || function() { window.callPhantom({ action: 'panic' }); };
             $('html')
                 .on('pdf:docloaded', function(event) {
                     window.callPhantom({ action: 'finished' });
@@ -23,6 +22,7 @@ function finishLoad(status) {
 }
 
 function writePage(pageIndex, html) {
+    var fs = require('fs');
     var css = '<link href="viewer.css" rel="stylesheet" />';
     var head = '<head>' + css + '</head>\n';
     var content = '<doctype html>\n<html>\n' + head + '<body>\n' + html + '\n</body>\n</html>\n';
@@ -43,64 +43,6 @@ page.onCallback = function(cb) {
             console.log('page.onCallback: unknown action received (' + cb.action + ')');
             phantom.exit();
     }
-};
-
-page.onConsoleMessage = function(msg, lineNum, sourceId) {
-    if (typeof lineNum === 'undefined') {
-        console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
-    } else {
-        console.log('CONSOLE: ' + msg);
-    }
-};
-
-page.onResourceRequested = function (request) {
-    system.stderr.writeLine('= onResourceRequested()');
-    system.stderr.writeLine('  request: ' + JSON.stringify(request, undefined, 4));
-};
- 
-page.onResourceReceived = function(response) {
-    system.stderr.writeLine('= onResourceReceived()' );
-    system.stderr.writeLine('  id: ' + response.id + ', stage: "' + response.stage + '", response: ' + JSON.stringify(response));
-};
- 
-page.onLoadStarted = function() {
-    system.stderr.writeLine('= onLoadStarted()');
-    var currentUrl = page.evaluate(function() {
-        return window.location.href;
-    });
-    system.stderr.writeLine('  leaving url: ' + currentUrl);
-};
- 
-page.onLoadFinished = function(status) {
-    system.stderr.writeLine('= onLoadFinished()');
-    system.stderr.writeLine('  status: ' + status);
-    finishLoad(status);
-};
- 
-page.onNavigationRequested = function(url, type, willNavigate, main) {
-    system.stderr.writeLine('= onNavigationRequested');
-    system.stderr.writeLine('  destination_url: ' + url);
-    system.stderr.writeLine('  type (cause): ' + type);
-    system.stderr.writeLine('  will navigate: ' + willNavigate);
-    system.stderr.writeLine('  from page\'s main frame: ' + main);
-};
- 
-page.onResourceError = function(resourceError) {
-    system.stderr.writeLine('= onResourceError()');
-    system.stderr.writeLine('  - unable to load url: "' + resourceError.url + '"');
-    system.stderr.writeLine('  - error code: ' + resourceError.errorCode + ', description: ' + resourceError.errorString );
-};
- 
-page.onError = function(msg, trace) {
-    system.stderr.writeLine('= onError()');
-    var msgStack = ['  ERROR: ' + msg];
-    if (trace) {
-        msgStack.push('  TRACE:');
-        trace.forEach(function(t) {
-            msgStack.push('    -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
-        });
-    }
-    system.stderr.writeLine(msgStack.join('\n'));
 };
 
 page.open('http://localhost:8080/pdf/tracemonkey.pdf');
