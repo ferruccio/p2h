3203cfdd73cd456e32c7d6dd8f056ef8199c861e
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2017-04-25 07:47:08 -0400

    Use command line arguments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs-jetpack');
+const path = require('path');
+
 const Nightmare = require('nightmare');
 const JSZip = require('jszip');
+const program = require('commander');
 
 let nightmare = Nightmare({
     show: false,
@@ -8,31 +11,43 @@ let nightmare = Nightmare({
     waitTimeout: 10000
 });
 let source = fs.path(fs.cwd(), 'page.html');
-let pdf = fs.path(fs.cwd(), 'tracemonkey.pdf');
 
-nightmare
-    .on('console', (type, msg) => console.log(type + '> ' + msg))
-    .goto('file://' + source + '?pdf=' + encodeURIComponent('file://' + pdf))
-    .wait('#done')
-    .evaluate(() => {
-        let pages = [];
-        let npages = $('.page').length;
-        for (let pi = 0; pi < npages; ++pi)
-            pages.push($('#page-index-' + pi).html());
-        return pages;
-    })
-    .end()
-    .then(pages => {
-        let zip = JSZip();
-        let template = fs.read('page-template.html');
-        for (let pi = 0; pi < pages.length; ++pi) {
-            zip.file('page-' + (pi+1) + '.html', template.replace('{{page}}', pages[pi]));
-            console.log('page: ' + (pi + 1));
-        }
-        zip .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-            .pipe(fs.createWriteStream(pdf + '.zip'))
-            .on('finish', function () {
-                console.log("zip finished.");
-            });
-    })
-    .catch(error => console.error(error));
+function p2h(pdf) {
+    nightmare
+        .on('console', (type, msg) => console.log(type + '> ' + msg))
+        .goto('file://' + source + '?pdf=' + encodeURIComponent('file://' + pdf))
+        .wait('#done')
+        .evaluate(() => {
+            let pages = [];
+            let npages = $('.page').length;
+            for (let pi = 0; pi < npages; ++pi)
+                pages.push($('#page-index-' + pi).html());
+            return pages;
+        })
+        .end()
+        .then(pages => {
+            let zip = JSZip();
+            let template = fs.read('page-template.html');
+            for (let pi = 0; pi < pages.length; ++pi) {
+                zip.file('page-' + (pi+1) + '.html', template.replace('{{page}}', pages[pi]));
+                console.log('page: ' + (pi + 1));
+            }
+            zip .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+                .pipe(fs.createWriteStream(pdf + '.zip'))
+                .on('finish', function () {
+                    console.log(pdf + ".zip written");
+                });
+        })
+        .catch(error => console.error(error));
+}
+
+program
+    .version('0.0.1')
+    .arguments('<source>')
+    .action(source => {
+        source = path.resolve(path.normalize(source));
+        console.log('processing: ' + source);
+        p2h(source);
+    });
+
+program.parse(process.argv);
